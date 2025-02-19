84de569273060866fc6ade3ed41e53ceef2b1f9d
Trice97 <bolinp@gmail.com>
2025-02-19 23:14:29 +0100

    05

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nbr;
+
+	for (nbr = 0 ; nbr <= 9; nbr++)
+
+{
+	printf("%d", nbr);
+}
+	printf("\n");
+
+	return (0);
+}
