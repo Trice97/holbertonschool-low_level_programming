e49acef07d74d17122d8f5afaecee6b2f1aa3da8
Trice97 <bolinp@gmail.com>
2025-02-19 22:28:23 +0100

    04

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
