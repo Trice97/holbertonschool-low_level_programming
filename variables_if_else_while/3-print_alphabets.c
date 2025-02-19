a2d159878d122f9f8318f02548a3497d2f46bc17
Trice97 <bolinp@gmail.com>
2025-02-19 15:26:58 +0100

    03

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter , LETTER;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+
+	}
+
+
+
+
+	for (LETTER = 'A'; LETTER <= 'Z'; LETTER++)
+	{
+			putchar(LETTER);
+	}
+	putchar('\n');
+	return (0);
+}
