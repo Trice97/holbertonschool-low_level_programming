b7dbef00a86a14b81a4c15af99bc81849ec1cc31
Trice97 <bolinp@gmail.com>
2025-02-23 19:48:10 +0100

    functions_nested_loops-0

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	char h;
 
-	for (h = '0'; h <= '9'; h++)
+	for (h = 'a'; h <= 'f'; h++)
 	{
 		putchar(h);
 	}
 
-	for (h = 'a'; h <= 'f'; h++)
+	for (h = '0'; h <= '9'; h++)
 	{
 		putchar(h);
 	}
