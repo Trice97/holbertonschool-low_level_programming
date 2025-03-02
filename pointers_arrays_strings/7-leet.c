eacba373954f32301b99181e183b7c7c8bab5ab5
Trice97 <bolinp@gmail.com>
2025-03-02 23:37:56 +0100

    7.7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @str: Pointer to the string.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char leet_chars[] = "aAeEoOtTlL";
+	char leet_replacements[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	for (j = 0; leet_chars[j] != '\0'; j++)
+	{
+	if (str[i] == leet_chars[j])
+	{
+	str[i] = leet_replacements[j];
+	break;
+	}
+	}
+	}
+
+	return (str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -36,7 +36,7 @@ char *string_toupper(char *);
 
 char *cap_string(char *);
 
-
+char *leet(char *);
 
 
 
