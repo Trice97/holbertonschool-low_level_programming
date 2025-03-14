0deba862a6130a984f82f01ee807870f8d488845
Trice97 <bolinp@gmail.com>
2025-03-14 21:32:44 +0100

    dog.h

--- structures_type/dog.h
+++ structures_type/dog.h
@@ -7,14 +7,12 @@
  * @age: Age of the dog
  * @owner: Owner of the dog
  */
-
-struct dog
-{
-		char *name;
-		float age;
-		char *owner;
+struct dog {
+        char *name;
+        float age;
+        char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#endif 
+#endif
