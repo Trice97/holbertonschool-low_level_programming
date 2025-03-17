d8014270b0548dbd8eabdab4bd4445ce9af5ef4d
Trice97 <bolinp@gmail.com>
2025-03-17 23:36:15 +0100

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,53 +1,64 @@
-#include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+#include "dog.h"
 
 /**
- * new_dog - Creates a new dog.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
- *
- * Return: A pointer to the new dog, or NULL if the function fails.
+ * _strdup - Duplicates a string
+ * @str: The string to duplicate
+ * Return: Pointer to the duplicated string, or NULL on failure
  */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, len = 0;
 
+	if (!str)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		dup[i] = str[i];
+
+	return (dup);
+}
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: Pointer to new dog, or NULL on failure
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *name_copy;
-	char *owner_copy;
+	dog_t *dog;
 
-if (name == NULL || owner == NULL)
-{
-	return (NULL);
-}
+	if (!name || !owner)
+		return (NULL);
 
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
-{
-	return (NULL);
-}
+	dog = malloc(sizeof(dog_t));
+	if (!dog)
+		return (NULL);
 
-name_copy = malloc(strlen(name) + 1);
-if (name_copy == NULL)
-{
-	free(new_dog);
-	return (NULL);
-}
-strcpy(name_copy, name);
+	dog->name = _strdup(name);
+	if (!(dog->name))
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = _strdup(owner);
+	if (!(dog->owner))
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->age = age;
 
-owner_copy = malloc(strlen(owner) + 1);
-if (owner_copy == NULL)
-{
-	free(name_copy);
-	free(new_dog);
-	return (NULL);
-}
-strcpy(owner_copy, owner);
-
-new_dog->name = name_copy;
-new_dog->age = age;
-new_dog->owner = owner_copy;
-
-return (new_dog);
+	return (dog);
 }
