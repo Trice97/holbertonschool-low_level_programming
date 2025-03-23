76e0c7934c07ebb3dc501f80f1c59af70a8f0e9a
Trice97 <bolinp@gmail.com>
2025-03-23 21:19:08 +0100

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -18,37 +18,31 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		switch (format[i])
 		{
-			printf("%s%c", separator, va_arg(args, int));
-			separator = ", ";
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%d", separator, va_arg(args, int));
-			separator = ", ";
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", separator, va_arg(args, double));
-			separator = ", ";
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", separator, str);
-			separator = ", ";
-		}
-		else
-		{
-
-			i++;
-			continue;
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				separator = ", ";
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				separator = ", ";
+				break;
+			case 'f':
+				printf("%s%d", separator, va_arg(args, int));
+				separator = ", ";
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				separator = ", ";
+				break;
+			default:
+				break;
 		}
 		i++;
-
 	}
 	printf("\n");
 	va_end(args);
